d329efa8c65697871e31e0b218042f91df182c98
iamraghavan <raghavanofficials@gmail.com>
2023-10-13 17:53:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000; // Set your desired port
+
+// Define an endpoint to serve your CSV data
+app.get('/api/data', (req, res) => {
+  const data = [];
+  fs.createReadStream('./blood-banks.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      data.push(row);
+    })
+    .on('end', () => {
+      res.json(data);
+    });
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
+
